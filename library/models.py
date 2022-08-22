f6eb1a71b7d2be06380f6dc2dfa346a26d62c349
Stanislav Khramtsov <sauron4er@gmail.com>
2022-08-22 13:44:50 +0300

    bd work

--- library/models.py
+++ library/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.db.models import URLField
 
-# Create your models here.
+
+class Comic(models.Model):
+    name = models.CharField(max_length=30)
+    rating = models.DecimalField(max_digits=2, decimal_places=1)  # до 1 000 000
+    born = models.DateField(null=True)
+    died = models.DateField(null=True)
+    picture = models.ImageField(upload_to='comics')
+    wiki_url = URLField(max_length=200)
+    is_active = models.BooleanField(default=True)
+
+
+class Streaming(models.Model):
+    name = models.CharField(max_length=30)
+    web_address = URLField(max_length=200)
+    is_active = models.BooleanField(default=True)
+
+
+class Special(models.Model):
+    name = models.CharField(max_length=30)
--- profile/models.py
+++ profile/models.py
@@ -8,27 +8,26 @@ class Theme(models.Model):
     is_active = models.BooleanField(default=True)
 
 
-class Theme(models.Model):
-    name = models.CharField(max_length=6)
-    is_navbar_dark = models.BooleanField(default=False)
+class Language(models.Model):
+    name = models.CharField(max_length=10)
     is_active = models.BooleanField(default=True)
 
 
 class Country(models.Model):
-    name = models.CharField(max_length=6)
-    flag = models.BooleanField(default=False)
+    name = models.CharField(max_length=25)
+    flag = models.ImageField(upload_to='flags')
     main_language = models.ForeignKey(Language, related_name='countries', on_delete=models.RESTRICT)
     is_active = models.BooleanField(default=True)
 
 
 class Account(models.Model):
     user = models.OneToOneField(User, on_delete=models.RESTRICT)
-    name = models.CharField(max_length=100)
     phone = models.CharField(max_length=10, null=True)
     country = models.ForeignKey(Country, related_name='users', on_delete=models.RESTRICT)
     theme = models.ForeignKey(Theme, related_name='users', on_delete=models.RESTRICT)
-    note = models.CharField(max_length=1000, null=True)
+    language = models.ForeignKey(Language, related_name='countries', on_delete=models.RESTRICT)
+    note = models.TextField(max_length=1000, null=True)
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
-        return self.name
+        return self.user.username
